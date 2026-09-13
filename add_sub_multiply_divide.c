3c5da6057b19bdce3cfe7a6f4d7c9342fbac04ff
Rishikesh-codes <7a.rishikeshpalla@gmail.com>
2026-09-13 23:48:54 +0530

    Add files via upload

---
+++ add_sub_multiply_divide.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int a=50,b=50;
+    printf("Sum of a and b is:%d\n",a+b);
+    printf("Sub of a and b is:%d\n",a-b);
+    printf("Multiply a and b is:%d\n",a*b);
+    printf("Divide a and b is:%d\n",a/b);
+}
